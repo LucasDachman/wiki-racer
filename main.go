722f1038a121e5ffabeaf70e067df4a779d73f80
Lucas Dachman <lucas.dachman@devetry.com>
2021-07-27 15:21:46 -0600

    create race function to crawl wiki

--- main.go
+++ main.go
@@ -1,12 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
-// https://en.wikipedia.org/w/api.php?action=query&titles=Albert%20Einstein&prop=links
+var visited sync.Map
+var wiki *WikiService
+
 func main() {
-	wiki := NewWikiService()
-	title := wiki.RandomPageTitle()
-	links := wiki.ListLinks(title, "")
-	fmt.Println(title)
-	fmt.Println(links)
+	wiki = NewWikiService()
+
+	title1 := wiki.RandomPageTitle()
+	title2 := wiki.RandomPageTitle()
+
+	fmt.Printf("Title1: %v, Title2: %v\n", title1, title2)
+
+	race(title1, title2)
+	// fmt.Println(visited)
+}
+
+func race(title1, title2 string) {
+	fmt.Printf("Visiting %v\n", title1)
+	links := wiki.ListLinks(title1, "")
+	fmt.Printf("Got links for %v\n", title1)
+	for _, link := range links {
+		if link == title2 {
+			fmt.Printf("Found on %v\n", title1)
+			return
+		}
+		if _, ok := visited.LoadOrStore(link, true); !ok {
+			race(link, title2)
+		}
+	}
 }
